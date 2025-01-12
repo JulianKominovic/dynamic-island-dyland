53c2b9a52b5680b3b80c6a74d35a82806ea29b11
JulianKominovic <juliankominovic@gmail.com>
2025-01-12 12:37:14 -0300

    chore: update extension name

--- gnome-extension/launch.sh
+++ gnome-extension/launch.sh
@@ -1,3 +1,3 @@
-cp -r ./gnome-extension/src ~/.local/share/gnome-shell/extensions/
+cp -r ./gnome-extension/src ~/.local/share/gnome-shell/extensions/dynamic-island-ubuntu@julian
 gnome-extensions enable dynamic-island-ubuntu@julian
 killall -3 gnome-shell
