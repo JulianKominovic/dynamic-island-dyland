ae8640fcae3b296ed7932f180ff2280f8ea5097f
JulianKominovic <juliankominovic@gmail.com>
2025-01-12 12:34:00 -0300

    feat: gnome extension

---
+++ gnome-extension/launch.sh
@@ -0,0 +1,3 @@
+cp -r ./gnome-extension/src ~/.local/share/gnome-shell/extensions/
+gnome-extensions enable dynamic-island-ubuntu@julian
+killall -3 gnome-shell
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -33,43 +33,6 @@ fn main() {
                     let _: () = msg_send![ns_window, setLevel: MACOS_WINDOW_LEVEL];
                 }
             }
-            #[cfg(target_os = "linux")]
-            {
-                let x11_window_id = window.gtk_window().unwrap() as u64;
-
-                unsafe {
-                    let display = xlib::XOpenDisplay(ptr::null());
-                    let root = xlib::XDefaultRootWindow(display);
-
-                    let atom = xlib::XInternAtom(
-                        display,
-                        b"_NET_WM_WINDOW_TYPE\0".as_ptr() as *const i8,
-                        xlib::False,
-                    );
-                    let value = xlib::XInternAtom(
-                        display,
-                        b"_NET_WM_WINDOW_TYPE_NOTIFICATION\0".as_ptr() as *const i8,
-                        xlib::False,
-                    );
-
-                    xlib::XChangeProperty(
-                        display,
-                        x11_window_id,
-                        atom,
-                        xlib::XA_ATOM,
-                        32,
-                        xlib::PropModeReplace,
-                        &value as *const xlib::Atom as *const u8,
-                        1,
-                    );
-
-                    // Mueve la ventana al frente
-                    xlib::XRaiseWindow(display, x11_window_id);
-
-                    xlib::XFlush(display);
-                    xlib::XCloseDisplay(display);
-                }
-            }
 
             if let Err(e) = window.set_position(tauri::PhysicalPosition::new(
                 screen_x_center as i32 - MACOS_NOTCH_WIDTH_PX / 2,
@@ -86,42 +49,6 @@ fn main() {
 
             Ok(())
         })
-        // .setup(|app| {
-        //     let window = app.get_webview_window("main").unwrap();
-        //     // Obtén el ID de la ventana
-        //     let gdk_window = window.gtk_window().unwrap();
-        //     println!("GDK Window: {:?}", gdk_window);
-        //     // unsafe {
-        //     //     let display = xlib::XOpenDisplay(ptr::null());
-        //     //     let root = xlib::XDefaultRootWindow(display);
-        //     //     // Configura el tipo de ventana
-        //     //     let atom = xlib::XInternAtom(
-        //     //         display,
-        //     //         b"_NET_WM_WINDOW_TYPE\0".as_ptr() as *const u8,
-        //     //         xlib::False,
-        //     //     );
-        //     //     let value = xlib::XInternAtom(
-        //     //         display,
-        //     //         b"_NET_WM_WINDOW_TYPE_NOTIFICATION\0".as_ptr() as *const u8,
-        //     //         xlib::False,
-        //     //     );
-        //     //     xlib::XChangeProperty(
-        //     //         display,
-        //     //         x11_window_id,
-        //     //         atom,
-        //     //         xlib::XA_ATOM,
-        //     //         32,
-        //     //         xlib::PropModeReplace,
-        //     //         &value as *const xlib::Atom as *const u8,
-        //     //         1,
-        //     //     );
-        //     //     // Mueve la ventana al frente
-        //     //     xlib::XRaiseWindow(display, x11_window_id);
-        //     //     xlib::XFlush(display);
-        //     //     xlib::XCloseDisplay(display);
-        //     // }
-        //     Ok(())
-        // })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
