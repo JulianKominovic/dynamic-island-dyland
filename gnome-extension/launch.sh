130bf717a6ab6343ee2b5f729c8668268af65aa0
JulianKominovic <juliankominovic@gmail.com>
2025-01-12 16:02:00 -0300

    feat: linux x11 dynamic island window done

--- gnome-extension/launch.sh
+++
@@ -1,3 +0,0 @@
-cp -r ./gnome-extension/src/* ~/.local/share/gnome-shell/extensions/dynamic-island-ubuntu@julian/
-gnome-extensions enable dynamic-island-ubuntu@julian
-killall -3 gnome-shell
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,16 +3,19 @@
 
 pub mod events;
 use events::greet;
+use gtk::prelude::{GtkWindowExt, WidgetExt};
 use tauri::Manager;
 pub mod multimedia;
 #[cfg(target_os = "macos")]
 use cocoa::base::id;
+#[cfg(target_os = "linux")]
+use gtk::{
+    gdk::WindowTypeHint,
+    prelude::{BinExt, ContainerExt, GtkWindowExt, WidgetExt},
+};
 #[cfg(target_os = "macos")]
 use objc::{msg_send, sel, sel_impl};
 use std::ptr;
-#[cfg(target_os = "linux")]
-use x11::xlib;
-
 const MACOS_NOTCH_HEIGHT_PX: i32 = 64;
 const MACOS_WINDOW_LEVEL: u8 = 25;
 const MACOS_NOTCH_WIDTH_PX: i32 = 360;
@@ -32,19 +35,44 @@ fn main() {
                 unsafe {
                     let _: () = msg_send![ns_window, setLevel: MACOS_WINDOW_LEVEL];
                 }
+                if let Err(e) = window.set_position(tauri::PhysicalPosition::new(
+                    screen_x_center as i32 - MACOS_NOTCH_WIDTH_PX / 2,
+                    0,
+                )) {
+                    eprintln!("Failed to set window position: {:?}", e);
+                }
+                if let Err(e) = window.set_size(tauri::PhysicalSize::new(
+                    MACOS_NOTCH_WIDTH_PX,
+                    MACOS_NOTCH_HEIGHT_PX,
+                )) {
+                    eprintln!("Failed to set window size: {:?}", e);
+                }
             }
 
-            if let Err(e) = window.set_position(tauri::PhysicalPosition::new(
-                screen_x_center as i32 - MACOS_NOTCH_WIDTH_PX / 2,
-                0,
-            )) {
-                eprintln!("Failed to set window position: {:?}", e);
-            }
-            if let Err(e) = window.set_size(tauri::PhysicalSize::new(
-                MACOS_NOTCH_WIDTH_PX,
-                MACOS_NOTCH_HEIGHT_PX,
-            )) {
-                eprintln!("Failed to set window size: {:?}", e);
+            #[cfg(target_os = "linux")]
+            {
+                let gtk_window = window.gtk_window().unwrap();
+                if let Some(vbox) = gtk_window.get_child() {
+                    gtk_window.remove(&vbox);
+                    let new_window = gtk::Window::new(gtk::WindowType::Popup);
+                    new_window.set_type_hint(gdk::WindowTypeHint::Dock);
+                    new_window.set_resizable(false);
+                    new_window.set_decorated(false);
+                    new_window.set_keep_above(true);
+                    new_window.set_skip_taskbar_hint(true);
+                    new_window.set_skip_pager_hint(true);
+
+                    new_window.add(&vbox);
+                    new_window.set_default_size(MACOS_NOTCH_WIDTH_PX, MACOS_NOTCH_HEIGHT_PX);
+                    new_window.set_size_request(MACOS_NOTCH_WIDTH_PX, MACOS_NOTCH_HEIGHT_PX);
+                    new_window.move_(
+                        (screen_x_center as i32 - MACOS_NOTCH_WIDTH_PX / 2) as i32,
+                        0,
+                    );
+                    new_window.show_all();
+                    vbox.show_all();
+                    window.hide().unwrap();
+                }
             }
 
             Ok(())
